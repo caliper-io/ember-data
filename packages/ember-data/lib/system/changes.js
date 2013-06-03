47c29ffbb75261d83fa5aa5555f15399eec5e58f
Godfrey Chan <godfreykfc@gmail.com>
2013-06-03 10:51:51 -0700

    Build ember-data-source gem v0.13

--- packages/ember-data/lib/system/changes.js
+++
@@ -1,7 +0,0 @@
-/**
-  @module data
-  @submodule data-changes
-*/
-
-require("ember-data/system/changes/attribute_change");
-require("ember-data/system/changes/relationship_change");
--- packages/ember-data/lib/system/relationships.js
+++
@@ -1,8 +0,0 @@
-/**
-  @module data
-  @submodule data-relationships
-*/
-
-require("ember-data/system/relationships/belongs_to");
-require("ember-data/system/relationships/has_many");
-require("ember-data/system/relationships/ext");
